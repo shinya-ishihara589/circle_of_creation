bb92ea41c45ec5c92717c36b99ba41168eeb7070
ishihara_shinya <s.ishihara5899@gmail.com>
2025-12-02 09:31:26 +0900

    no message

---
+++ resources/views/game.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head></head>
+
+<body>
+    <canvas id="game_anvas" width="400" height="200" style="border:1px solid #000;"></canvas>
+    <script>
+        console.log(1);
+    </script>
+</body>
+
+</html>
