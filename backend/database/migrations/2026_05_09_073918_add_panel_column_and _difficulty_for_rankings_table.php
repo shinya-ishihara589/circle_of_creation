84e2cff93551821361b8fe16cb3882678ba6d7b0
ishihara_shinya <s.ishihara5899@gmail.com>
2026-05-09 07:45:52 +0900

    マイグレーションを修正

---
+++ backend/database/migrations/2026_05_09_073918_add_panel_column_and _difficulty_for_rankings_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('rankings', function (Blueprint $table) {
+            $table->string('difficulty', 10)->nullable()->after('ip_address');
+            $table->tinyInteger('panel')->default(5)->after('difficulty');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('rankings', function (Blueprint $table) {
+            $table->dropColumn('panel');
+            $table->dropColumn('difficulty');
+        });
+    }
+};
